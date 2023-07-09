aee5b881c685e0085e375ecb219a01b61f75d232
Armend Tahiraga <armend_tahiraga@outlook.com>
2023-07-09 22:14:05 +0200

    Made a few changes and added buttons for slides

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
-const slides = document.getElementsByClassName("carousel-item")
-const prevBtn = document.getElementById("prev-slide")
-const nextBtn = document.getElementById("next-slide")
-const totalSlides = slides.length
-let slidePosition = 0
+const slides = document.getElementsByClassName("carousel-item");
+const slideButtons = document.getElementsByClassName("slide-button");
+const prevBtn = document.getElementById("prev-slide");
+const nextBtn = document.getElementById("next-slide");
+const totalSlides = slides.length;
+let slidePosition = 0;
 
-prevBtn.addEventListener("click", moveToPrevSlide)
-nextBtn.addEventListener("click", moveToNextSlide)
+prevBtn.innerHTML = "<";
+prevBtn.addEventListener("click", moveToPrevSlide);
+nextBtn.innerHTML = ">";
+nextBtn.addEventListener("click", moveToNextSlide);
+
+for (const button of slideButtons) {
+	button.addEventListener("click", moveSlide);
+}
+
+function moveSlide(event) {
+	hideAllSlides();
+
+	slidePosition = JSON.parse(event.target.id);
+
+	slides[slidePosition].classList.add("carousel-item-visible");
+	slides[slidePosition].classList.remove("carousel-item-hidden");
+	slideButtons[slidePosition].classList.add("active");
+}
 
 function moveToPrevSlide() {
-   hideAllSlides()
+	hideAllSlides();
 
-   if (slidePosition === 0) {
-      slidePosition = totalSlides - 1
-   } else {
-      slidePosition--
-   }
-   slides[slidePosition].classList.add("carousel-item-visible")
-   slides[slidePosition].classList.remove("carousel-item-hidden")
+	if (slidePosition === 0) {
+		slidePosition = totalSlides - 1;
+	} else {
+		slidePosition--;
+	}
+
+	slides[slidePosition].classList.add("carousel-item-visible");
+	slides[slidePosition].classList.remove("carousel-item-hidden");
+	slideButtons[slidePosition].classList.add("active");
 }
+
 function moveToNextSlide() {
-   hideAllSlides()
+	hideAllSlides();
 
-   if (slidePosition === totalSlides - 1) {
-      slidePosition = 0
-   } else {
-      slidePosition++
-   }
-   slides[slidePosition].classList.add("carousel-item-visible")
-   slides[slidePosition].classList.remove("carousel-item-hidden")
+	if (slidePosition === totalSlides - 1) {
+		slidePosition = 0;
+	} else {
+		slidePosition++;
+	}
+
+	slides[slidePosition].classList.add("carousel-item-visible");
+	slides[slidePosition].classList.remove("carousel-item-hidden");
+	slideButtons[slidePosition].classList.add("active");
 }
+
 function hideAllSlides() {
-   for (let slide of slides) {
-      slide.classList.add("carousel-item-hidden")
-   }
+	for (const slide of slides) {
+		slide.classList.add("carousel-item-hidden");
+	}
+
+	for (const button of slideButtons) {
+		button.classList.remove("active");
+	}
 }
